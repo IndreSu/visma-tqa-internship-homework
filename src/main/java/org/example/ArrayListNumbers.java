e204e525cd99df4fe989d97407ac3de61a9699ee
Indre <indre.bitinaite@gmail.com>
2023-10-05 21:34:19 +0300

    Homework task changes1

--- src/main/java/org/example/ArrayListNumbers.java
+++ src/main/java/org/example/ArrayListNumbers.java
@@ -6,7 +6,7 @@ public class ArrayListNumbers {
 
     public static void showArrayListNumbers(ArrayList<Integer> listNumbers) {
         for (int i = 0; i < listNumbers.size(); i++) {
-            System.out.print(listNumbers.get(i) + ", ");
+            System.out.println(listNumbers.get(i));
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,16 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<Integer>();
+        LocalDate today = LocalDate.now();
+        System.out.println("Today is day "+ today.getDayOfWeek().getValue());
+        System.out.println("Please enter a whole integer (0 to stop):");
 
         while (true) {
-            System.out.println("Please enter a whole integer (enter 0 to stop):");
             try {
             int number = Integer.parseInt(scanner.nextLine());
                 if (number == 0) {
@@ -19,13 +20,11 @@ public class Main {
                 }
                 numbers.add(number);
             } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a valid whole number.");
+                System.out.println("Invalid input. Please enter a valid whole number!");
             }
         }
 
         int sum = ArrayListSum.calculateArrayListNumbers(numbers);
-
-        LocalDate today = LocalDate.now();
 
         int sumDividedDayToday = sum / today.getDayOfWeek().getValue();
         System.out.println("Sum of all numbers divided by a number of today's day is: " + sumDividedDayToday);
@@ -33,14 +32,21 @@ public class Main {
         System.out.println("Choose menu item:");
         System.out.println("1. The size of the array");
         System.out.println("2. The numbers of the array");
+        System.out.println("0. Exit the program");
 
-        int menuItem = Integer.parseInt(scanner.nextLine());
-        if (menuItem == 1) {
-            System.out.println("The size of the array is:" + ArrayListSize.showArrayListSize(numbers));
-        } else if (menuItem == 2) {
-            System.out.println("The numbers of the array are: ");
-            ArrayListNumbers.showArrayListNumbers(numbers);
-        }
+        while (true) {
+            int menuItem = Integer.parseInt(scanner.nextLine());
+                if (menuItem == 0) {
+                    break;
+                } else if (menuItem == 1) {
+                    System.out.println("The size of the array is: " + ArrayListSize.showArrayListSize(numbers));
+                } else if (menuItem == 2) {
+                    System.out.println("The numbers of the array are: ");
+                    ArrayListNumbers.showArrayListNumbers(numbers);
+                } else {
+                    System.out.println("Invalid input. Please enter a valid number (0-2)!");
+                }
+            }
         scanner.close();
+        }
     }
-}
